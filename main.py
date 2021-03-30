1d24fed8934b11119684bf8ff859fe43c6e511da
max <max@ludd.ltu.se>
2021-03-30 13:21:46 +0200

    More updates

--- main.py
+++ main.py
@@ -28,33 +28,45 @@ def main():
     nrf24 = NRF24L01P(io_ctrl.spi_transfer, io_ctrl.a_pin)
 
     if sys.platform in 'darwin':  # MacBook Pro
-        prx_mode(nrf24, rf_ch, payload_size)
-    else:  # Raspberry Pi or MicroPython
         ptx_mode(nrf24, rf_ch, payload_size)
+    else:  # Raspberry Pi or MicroPython
+        prx_mode(nrf24, rf_ch, payload_size)
 
 
 def prx_mode(nrf24, rf_ch, payload_size):
     print("Receiver mode")
 
-    nrf24.setup(prim_rx=1, rf_ch=rf_ch, payload_size=payload_size)
+#    nrf24.setup(prim_rx=1, rf_ch=rf_ch, payload_size=payload_size)
 
-    # Enable auto acknowledgement data pipe 0
-    response = nrf24.read_reg(NRF24L01P.EN_AA, 1)
-    en_ea = response[1] & NRF24L01P.ENAA_P0
-    nrf24.write_reg(NRF24L01P.EN_AA, bytes([en_ea]))
+    ic(nrf24.read_reg(NRF24L01P.STATUS, 1))
+    ic(nrf24.read_reg(NRF24L01P.OBSERVE_TX, 1))
+    print_fifo_status(nrf24)
+    print("Configure 'all' registers")
+    nrf24.write_cmd(NRF24L01P.FLUSH_RX)
+    nrf24.write_cmd(NRF24L01P.FLUSH_TX)
+    nrf24.write_reg(NRF24L01P.STATUS, bytes([0b01110000]))  # Clear any pending interrupts
+    nrf24.write_reg(NRF24L01P.CONFIG, bytes([0b01111111]))  # PRX
+    nrf24.write_reg(NRF24L01P.EN_AA, bytes([0b00111111]))
+    nrf24.write_reg(NRF24L01P.EN_RXADDR, bytes([0b00000011]))
+    nrf24.write_reg(NRF24L01P.SETUP_AW, bytes([0b00000011]))
+    nrf24.write_reg(NRF24L01P.SETUP_RETR, bytes([0b00000011]))
+    nrf24.write_reg(NRF24L01P.RF_CH, bytes([2]))
+    nrf24.write_reg(NRF24L01P.RF_SETUP, bytes([0b00001110]))
+    ic(nrf24.read_reg(NRF24L01P.STATUS, 1))
+    ic(nrf24.read_reg(NRF24L01P.OBSERVE_TX, 1))
+    print_fifo_status(nrf24)
+    ic(nrf24.read_reg(NRF24L01P.RX_ADDR_P0, 5))
+    ic(nrf24.read_reg(NRF24L01P.TX_ADDR, 5))
+    ic(nrf24.read_reg(NRF24L01P.DYNPD, 1))
+    ic(nrf24.read_reg(NRF24L01P.FEATURE, 1))
 
-    # Enable data pipe 0
-    response = nrf24.read_reg(NRF24L01P.EN_RXADDR, 1)
-    en_rxaddr = response[1] & NRF24L01P.ERX_P0
-    nrf24.write_reg(NRF24L01P.EN_RXADDR, bytes([en_rxaddr]))
-
-    nrf24.power_up()  # Enter 'Standby-1'
+#    nrf24.power_up()  # Enter 'Standby-1'
     nrf24.trx_enable()
 
     ic(nrf24.status())
     ic(nrf24.read_reg(NRF24L01P.CONFIG, 1))
 
-    for i in range(10):
+    for i in range(5):
         print_fifo_status(nrf24)
         ic(nrf24.read_reg(NRF24L01P.RX_PW_P0, 1))
         time.sleep(1)
@@ -65,12 +77,29 @@ def ptx_mode(nrf24, rf_ch, payload_size):
 
     nrf24.setup(prim_rx=0, rf_ch=rf_ch, payload_size=payload_size, arc=15)
 
-    # Enable auto acknowledgement on pipe 0
-    response = nrf24.read_reg(NRF24L01P.EN_AA, 1)
-    en_ea = response[1] | NRF24L01P.ENAA_P0
-    nrf24.write_reg(NRF24L01P.EN_AA, bytes([en_ea]))
+    ic(nrf24.read_reg(NRF24L01P.STATUS, 1))
+    ic(nrf24.read_reg(NRF24L01P.OBSERVE_TX, 1))
+    print_fifo_status(nrf24)
+    print("Configure 'all' registers")
+    nrf24.write_cmd(NRF24L01P.FLUSH_RX)
+    nrf24.write_cmd(NRF24L01P.FLUSH_TX)
+    nrf24.write_reg(NRF24L01P.STATUS, bytes([0b01110000]))  # Clear any pending interrupts
+    nrf24.write_reg(NRF24L01P.CONFIG, bytes([0b01111110]))  # TRX
+    nrf24.write_reg(NRF24L01P.EN_AA, bytes([0b00111111]))
+    nrf24.write_reg(NRF24L01P.EN_RXADDR, bytes([0b00000011]))
+    nrf24.write_reg(NRF24L01P.SETUP_AW, bytes([0b00000011]))
+    nrf24.write_reg(NRF24L01P.SETUP_RETR, bytes([0b00000011]))
+    nrf24.write_reg(NRF24L01P.RF_CH, bytes([2]))
+    nrf24.write_reg(NRF24L01P.RF_SETUP, bytes([0b00001110]))
+    ic(nrf24.read_reg(NRF24L01P.STATUS, 1))
+    ic(nrf24.read_reg(NRF24L01P.OBSERVE_TX, 1))
+    print_fifo_status(nrf24)
+    ic(nrf24.read_reg(NRF24L01P.RX_ADDR_P0, 5))
+    ic(nrf24.read_reg(NRF24L01P.TX_ADDR, 5))
+    ic(nrf24.read_reg(NRF24L01P.DYNPD, 1))
+    ic(nrf24.read_reg(NRF24L01P.FEATURE, 1))
 
-    nrf24.power_up()  # Enter 'Standby-1'
+#    nrf24.power_up()  # Enter 'Standby-1'
 
     ic(nrf24.status())
 
